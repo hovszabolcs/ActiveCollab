f80a0af1828e8902ccb2cafd811a92b142053d10
Szabolcs Hovanecz <hovszabolcs@gmail.com>
2024-06-06 13:28:28 +0200

    Fixed appController

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use App\Helper\MessageType;
-use App\Services\AuthService;
 use App\Services\ActiveCollab\ActiveCollabAuthService;
-use App\Services\RedisCache;
+use App\Services\AuthService;
+use App\Services\CacheService\CacheInterface;
+use App\Services\CacheService\RedisCache;
 use Core\ConfigurationInterface;
 use Core\Controller;
 use Core\HtmlView;
@@ -15,19 +16,17 @@ use Core\Route\RouteItemInfo;
 // all public controller may extends from AppController
 abstract class AppController extends Controller
 {
+    protected CacheInterface $cache;
     protected AuthService $authService;
     protected ActiveCollabAuthService $apiService;
 
     public function __construct(ConfigurationInterface $config, Request $request, RouteItemInfo $routeItemInfo)
     {
-        $redis = new RedisCache('redis');
-
-        exit();
-
         parent::__construct($config, $request, $routeItemInfo);
 
         $this->apiService = new ActiveCollabAuthService();
         $this->authService = new AuthService($request->session, $this->apiService);
+        $this->cache = $this->makeCacheHandlerInstance($config);
 
         if ($this->request->session->has('siteMessage')) {
             HtmlView::setParam('siteMessage', $this->request->session->pull('siteMessage'));
@@ -38,4 +37,12 @@ abstract class AppController extends Controller
         $this->request->session->set('siteMessage', ['type' => $type, 'text' => $message]);
     }
 
+    public function getCacheHandler(): CacheInterface {
+        return $this->cache;
+    }
+
+    protected function makeCacheHandlerInstance(ConfigurationInterface $config): CacheInterface {
+        $redisUrl = $config->getEnv('REDIS_URL');
+        return new RedisCache($redisUrl);
+    }
 }
