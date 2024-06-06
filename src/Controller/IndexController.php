a6f929de67fa03fae64400f343951567adb76bf3
Szabolcs Hovanecz <hovszabolcs@gmail.com>
2024-06-06 13:28:46 +0200

    Fixed indexController

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,9 +8,9 @@ use Core\TwigView;
 class IndexController extends AuthenticatedController
 {
     public function index() {
-        $projects =  $this->client->getProjects()->getJson();
-        $projects = ProjectDTO::fromArray($projects);
+        $projects = $this->client->getProjects();
+        $projectsDTO = ProjectDTO::fromArray($projects);
 
-        return TwigView::Create('projectlist.html.twig', ['projects' => $projects]);
+        return TwigView::Create('projectlist.html.twig', ['projects' => $projectsDTO]);
     }
 }
